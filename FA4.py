4385930720ad9d7fe5ee9f27aee1d903ac835ef2
IllidanLucisLecterSTria <illidanlucis.tria@smc.pshs.edu.ph>
2025-11-06 19:41:54 +0800

    Average Score Calculator
    The problem is about the user inputting the amount of students and subjects. Based on those numbers, each student will have as many inputs as the number of subjects. The inputs should be their score on a subject. The subjects are not specific, just called "score". After that, the average of each student is needed and summing them all up and finding the average of that based on the number of students. The code does exactly that.

---
+++ FA4.py
@@ -0,0 +1,14 @@
+stud = int(input("Enter number of students: "))
+subj = int(input("Enter number of subjects: "))
+class_score = 0
+for i in range (1,stud + 1):
+    total = 0
+    print(f"Student {i}")
+    for j in range (1, subj + 1):
+        score = int(input(f"Enter score {j}: "))
+        total += score
+    stud_avg = total/subj
+    print(f"Average for Student {i} = ", stud_avg)
+    class_score += stud_avg
+class_avg = class_score/stud
+print(f"Class Average = ", class_avg)
